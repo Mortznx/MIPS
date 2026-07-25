cf2ee53d98de78e352288dc54a65127fe075583e
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:15:50 +0330

    Update ForwardingUnit.v

--- src/Controller-and-hazard/ForwardingUnit.v
+++ src/Controller-and-hazard/ForwardingUnit.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    21:30:58 12/01/2025 
-// Design Name: 
-// Module Name:    ForwardingUnit 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ForwardingUnit(ex25,ex20,mem,wb,writemem,writewb,
     forwardA,forwardB);
 	input writemem,writewb;
