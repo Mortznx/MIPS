72a7dba5c81098970127c94de20a9106c1f4d1fc
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:19:07 +0330

    Update ALU_add.v

--- src/ID/ALU_add.v
+++ src/ID/ALU_add.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:21:02 12/08/2025 
-// Design Name: 
-// Module Name:    ALU_add 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module ALU_add (in0,in1,out
    );
 	input[31:0] in0,in1;
