ee41b34d6af0cf51ec9e919ef29ab35d003ee9a3
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:24:01 +0330

    Update WB.v

--- src/WB/WB.v
+++ src/WB/WB.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    22:04:50 01/02/2026 
-// Design Name: 
-// Module Name:    WB 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module WB(memToReg,mData,address,out
     );
 	
