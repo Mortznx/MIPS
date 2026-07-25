3a4982388867f6136d452b07a852cbf2930c234a
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:22:05 +0330

    Update PC.v

--- src/IF/PC.v
+++ src/IF/PC.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:50:20 12/08/2025 
-// Design Name: 
-// Module Name:    PC 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module PC(
 		pcWrite,clk,pc_next,pc_out);
 	
