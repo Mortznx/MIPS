26add8e7c93a8c44dc772b5ffdb2e5f9818cb1f1
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:18:25 +0330

    Update Mux2to1.v

--- src/EX/Mux2to1.v
+++ src/EX/Mux2to1.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:07:21 12/08/2025 
-// Design Name: 
-// Module Name:    Mux2to1 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Mux2to1 #(parameter W=32) (in0,in1,ctrl,out
     );
 	 
