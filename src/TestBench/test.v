7d1973bc41b428a06c3dc0965f28252a7f716da1
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:23:14 +0330

    Update test.v

--- src/TestBench/test.v
+++ src/TestBench/test.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    14:13:34 02/09/2026 
-// Design Name: 
-// Module Name:    test 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module test(
     );
 	reg clk;
