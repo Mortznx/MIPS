64a798f3cb1eca923b6dd439813522e83963e0f1
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:20:29 +0330

    Update IF.v

--- src/IF/IF.v
+++ src/IF/IF.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:41:04 12/08/2025 
-// Design Name: 
-// Module Name:    IF 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module IF(pc_new,jump_new,pcWrite,branch,data,clk
     );
 	input clk,pcWrite,branch;
