04851212aa7dc0d828b744fb566a13a2300cd88c
Mortznx <155534721+Mortznx@users.noreply.github.com>
2026-07-25 14:16:44 +0330

    Update ALUVerilog.v

--- src/EX/ALUVerilog.v
+++ src/EX/ALUVerilog.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    21:11:38 12/02/2025 
-// Design Name: 
-// Module Name:    ALUVerilog 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ALUVerilog(numA,numB,result,ctrl);
 	
 	input[31:0]  numA , numB;
